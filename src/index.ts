9c49f6231ce566a5af770cf5c7ddf87cd2e60072
P-Chan <hello@0x50.io>
2022-01-10 05:41:54 +0900

    feat!: add text support

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import { convert } from 'encoding-japanese'
 
 import { create10DigitsTrip, create12DigitsTrip, createRawKeyTrip } from './creators'
 
+type Options = Partial<{
+  hideWhitespace: boolean
+}>
+
 const rawKeyPettern = /^#[0-9A-Fa-f]{16}[.\/0-9A-Za-z]{0,2}$/
 
-export const createTrip = (key: string) => {
+const maskSpecialSymbols = (text: string) => text.replace(/★/g, '☆').replace(/◆/g, '◇')
+
+export const createTripByKey = (key: string) => {
   const encodedKeyString = convert(key, 'SJIS', 'UNICODE')
 
   // 10 桁トリップ
@@ -21,3 +27,26 @@ export const createTrip = (key: string) => {
   // 12 桁トリップ
   return create12DigitsTrip(key)
 }
+
+export const createTripByText = (text: string, options?: Options) => {
+  const indexOfSharp = (() => {
+    const indexOfHalfWidthSharp = text.indexOf('#')
+    const indexOfFullWidthSharp = text.indexOf('＃')
+
+    if (indexOfHalfWidthSharp >= 0) return indexOfHalfWidthSharp
+    if (indexOfFullWidthSharp >= 0) return indexOfFullWidthSharp
+
+    return -1
+  })()
+
+  if (indexOfSharp < 0) return maskSpecialSymbols(text)
+
+  const name = text.substr(0, indexOfSharp)
+  const key = text.substr(indexOfSharp + 1)
+
+  const whitespaceIfNeeded = options?.hideWhitespace ? '' : ' '
+
+  return `${maskSpecialSymbols(name)}${whitespaceIfNeeded}◆${createTripByKey(key)}`
+}
+
+export const createTrip = (text: string, options?: Options) => createTripByText(text, options)
